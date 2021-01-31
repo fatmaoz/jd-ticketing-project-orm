d1c78e54540a91cbf5eb7d4bb02bd7847af48be9
fatmaoz <fatmas199021@gmail.com>
2021-01-31 14:35:11 +0000

    Ticketing ORM

---
+++ src/main/java/com/cybertek/entity/BaseEntity.java
@@ -0,0 +1,32 @@
+package com.cybertek.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@NoArgsConstructor
+@Getter
+@Setter
+public class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDateTime insertDateTime;
+    private Long insertUserId;
+    private LocalDateTime lastUpdateDateTime;
+    private Long lastUpdateUserId;
+
+    public BaseEntity(Long id, LocalDateTime insertDateTime,
+                      Long insertUserId, LocalDateTime lastUpdateDateTime, Long lastUpdateUserId) {
+    }
+}
